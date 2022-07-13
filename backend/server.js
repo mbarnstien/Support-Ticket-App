42ac06da36a1bf625b280a3acf2492094c454af5
Meg <92758663+mbarnstien@users.noreply.github.com>
2022-07-13 11:54:00 -0700

    Failed to connect database

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 5000
 const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+
+// Connect to Database
+connectDB()
 
 const app = express()
 
