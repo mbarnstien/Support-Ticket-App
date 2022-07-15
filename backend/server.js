730b9c6e4b2d58207f9f307efdd2da8aac0bd227
Meg <92758663+mbarnstien@users.noreply.github.com>
2022-07-15 11:15:40 -0700

    Fixed connection and added register user

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
-const PORT = process.env.PORT || 5000
 const {errorHandler} = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
+const PORT = process.env.PORT || 5000
+
 
 // Connect to Database
 connectDB()
